4bb01727449c65b9e9158eca5ddef9c31cb1d2ed
Alex Skorulis <skorulis@gmail.com>
2017-09-10 17:07:07 +1000

    Reorganise resources

--- calc/reference.js
+++ calc/reference.js
@@ -2,6 +2,19 @@ const fs = require('fs');
 const skills = JSON.parse(fs.readFileSync('static/ref/skills.json', 'utf8'));
 const resources = JSON.parse(fs.readFileSync('static/ref/resources.json', 'utf8'));
 
+for(e of skills.elements) {
+  e.totalAttack = e.damageModifiers.reduce((total,amount) => total + amount )
+  e.totalDefense = skills.elements.reduce( (total,element) => {return total + element.damageModifiers[e.index]},0)
+  e.resources = []
+}
+
+for (var id in resources.elemental) {
+  var r = resources.elemental[id]
+  r.id = parseInt(id)
+  skills.elements[r.skill].resources.push(r)
+  
+}
+
 module.exports = {
   skills,
   resources
--- server/controllers/reference.js
+++ server/controllers/reference.js
@@ -3,17 +3,12 @@ const ref = require("../../calc/reference")
 
 module.exports = {
   skills:function(req,res) {
-    var obj = JSON.parse(fs.readFileSync('static/ref/skills.json', 'utf8'));
-    for(e of obj.elements) {
-      e.totalAttack = e.damageModifiers.reduce((total,amount) => total + amount )
-      e.totalDefense = obj.elements.reduce( (total,element) => {return total + element.damageModifiers[e.index]},0)
-    }
     const helpers = {
       percent:function(fraction) { return fraction * 100 + "%"},
       colorClass:function(fraction) {
         return "v" + (fraction * 100)
       }
     }
-    res.render('skills',{skills:obj,helpers,helpers})
+    res.render('skills',{skills:ref.skills,helpers,helpers})
   }
 }
