031dbb3c82887bd69cc546aed7546a2b549d8f71
Alex Skorulis <skorulis@gmail.com>
2017-09-21 18:44:02 +1000

    Various item crap

--- calc/avatar.js
+++ calc/avatar.js
@@ -3,8 +3,8 @@ const ref = require("./reference")
 module.exports = {
   updateStats:function(avatar) {
     avatar.level = avatar.skills.reduce((total,s) => {return total + s.level},0)
-    avatar.health = avatar.skills.reduce((total,s,index) => {return total + ref.getSkill(s.id).healthModifier * s.level},0)
-    avatar.speed = avatar.skills.reduce((total,s,index) => {return total + ref.getSkill(s.id).speedModifier * s.level},0)
+    avatar.health = avatar.skills.reduce((total,s,index) => {return total + ref.getSkill(s.id).healthModifier * s.level},10)
+    avatar.speed = avatar.skills.reduce((total,s,index) => {return total + ref.getSkill(s.id).speedModifier * s.level},100)
     return avatar
   }
 }
